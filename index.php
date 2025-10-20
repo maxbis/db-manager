ee33e79f82096cbcabc0d3e496aa42670b3c70bf
Max Bisschop <max@ffkijken.com>
2025-10-20 22:03:25 +0200

    check

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Index Page - Redirects to Database Manager
+ * 
+ * This file serves as the default entry point and redirects
+ * users to the main Database Manager interface.
+ */
+
+header('Location: database_manager.php');
+exit;
+
